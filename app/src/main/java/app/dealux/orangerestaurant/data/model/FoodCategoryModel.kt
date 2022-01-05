5f97ea0ffad46d3f1c861caa50b70713d45ad3da
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-04 23:40:35 -0300

    Update FoodCategoryModel.kt

--- app/src/main/java/app/dealux/orangerestaurant/data/model/FoodCategoryModel.kt
+++ app/src/main/java/app/dealux/orangerestaurant/data/model/FoodCategoryModel.kt
@@ -1,9 +1,7 @@
 package app.dealux.orangerestaurant.data.model
 
-import android.net.Uri
-
 data class FoodCategoryModel(
     val id: Int,
     val categoryName: String,
-    val categoryPhoto: Uri,
+    val categoryPhoto: String,
 )
