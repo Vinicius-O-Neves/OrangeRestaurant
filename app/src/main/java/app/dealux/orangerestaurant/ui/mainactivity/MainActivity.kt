81875e062962766a0ab81cd30897266d3a9ade27
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-04 23:41:28 -0300

    Added category card click listener from adapter

--- app/src/main/java/app/dealux/orangerestaurant/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/app/dealux/orangerestaurant/ui/mainactivity/MainActivity.kt
@@ -3,16 +3,22 @@ package app.dealux.orangerestaurant.ui.mainactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
+import android.view.View
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import app.dealux.orangerestaurant.R
-import app.dealux.orangerestaurant.adapter.FoodCategoryAdapter
+import app.dealux.orangerestaurant.adapter.category.FoodCategoryAdapter
+import app.dealux.orangerestaurant.data.model.FoodCategoryModel
 import app.dealux.orangerestaurant.data.retrofit.RetrofitInstance
 import app.dealux.orangerestaurant.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :
+    AppCompatActivity(),
+    View.OnClickListener,
+    FoodCategoryAdapter.MyOnClickListener {
 
     private var binding: ActivityMainBinding? = null
 
@@ -28,26 +34,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-        setupRecyclerView()
-        lifecycleScope.launchWhenCreated {
-            val response = try {
-                RetrofitInstance.api.getCategorys()
-            } catch (e: IOException) {
-                Log.e("Retrofit", "You might not have internet")
-                return@launchWhenCreated
-            }
-            if (response.isSuccessful && response.body() != null) {
-                foodCategoryAdapter.items = response.body()!!
-            } else {
-                Log.e("Retrofit", "Response not successful")
+        CoroutineScope(Dispatchers.Main.immediate).launch {
+            setupRecyclerView()
+            lifecycleScope.launchWhenCreated {
+                val response = try {
+                    RetrofitInstance.api.getCategorys()
+                } catch (e: IOException) {
+                    Log.d("Retrofit", "You might not have internet")
+                    return@launchWhenCreated
+                }
+                if (response.isSuccessful && response.body() != null) {
+                    Log.d("Retrofit", "data successfuly load")
+                    foodCategoryAdapter.setData(response.body()!!)
+                } else {
+                    Log.d("Retrofit", "Response not successful")
+                }
             }
         }
     }
 
-    private fun setupRecyclerView() = binding!!.rvCategorys.apply {
-        foodCategoryAdapter = FoodCategoryAdapter()
+    private suspend fun setupRecyclerView() = binding!!.rvCategorys.apply {
+        foodCategoryAdapter = FoodCategoryAdapter(this@MainActivity, this@MainActivity)
         adapter = foodCategoryAdapter
         layoutManager =
             LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+        this.startLayoutAnimation()
     }
+
+    override fun onClick(view: View) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCardClick(position: Int, items: List<FoodCategoryModel>) {
+    }
+
 }
