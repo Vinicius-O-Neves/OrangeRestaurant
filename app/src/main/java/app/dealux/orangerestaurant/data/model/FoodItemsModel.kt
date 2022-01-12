199764d913d3f58784feb3a133d4d8048a0f2522
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-12 12:11:41 -0300

    Update FoodItemsModel.kt

--- app/src/main/java/app/dealux/orangerestaurant/data/model/FoodItemsModel.kt
+++ app/src/main/java/app/dealux/orangerestaurant/data/model/FoodItemsModel.kt
@@ -12,4 +12,28 @@ data class FoodItemsModel(
     val imageDescription1: String,
     val imageDescription2: String,
     val serve: String,
-) : Serializable
+) : Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        if (javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as FoodItemsModel
+
+        if (id != other.id) {
+            return false
+        }
+        if (category != other.category) {
+            return false
+        }
+        if (price != other.price) {
+            return false
+        }
+        if (frontCoverUrl != other.frontCoverUrl ) {
+            return false
+        }
+        return true
+    }
+
+}
