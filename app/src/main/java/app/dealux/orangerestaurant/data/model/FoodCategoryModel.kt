3a5cd552154ba06de53c7b479255b1b629be8988
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-12 12:11:38 -0300

    Update FoodCategoryModel.kt

--- app/src/main/java/app/dealux/orangerestaurant/data/model/FoodCategoryModel.kt
+++ app/src/main/java/app/dealux/orangerestaurant/data/model/FoodCategoryModel.kt
@@ -1,10 +1,30 @@
 package app.dealux.orangerestaurant.data.model
 
-import android.os.Parcelable
 import java.io.Serializable
 
 data class FoodCategoryModel(
     val id: Int,
     val categoryName: String,
     val categoryPhoto: String,
-) : Serializable
+) : Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        if (javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as FoodCategoryModel
+
+        if (id != other.id) {
+            return false
+        }
+        if (categoryName != other.categoryName) {
+            return false
+        }
+        if (categoryPhoto != other.categoryPhoto) {
+            return false
+        }
+        return true
+    }
+
+}
