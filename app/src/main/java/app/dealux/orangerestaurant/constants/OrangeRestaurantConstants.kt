5fe0721ee91a5d72737b45ee92e82f6a4511305b
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-12 12:10:52 -0300

    Class OrangeResturantConstants configured

---
+++ app/src/main/java/app/dealux/orangerestaurant/constants/OrangeRestaurantConstants.kt
@@ -0,0 +1,15 @@
+package app.dealux.orangerestaurant.constants
+
+class OrangeRestaurantConstants private constructor() {
+
+    object AUTH {
+        const val TYPE_OF_AUTH_PREFERENCE = "type_of_auth_preference"
+        const val AUTH_PREFERENCE_KEY =  "154"
+    }
+
+    object RECYCLERVIEW_LOAD_MORE {
+        const val VIEW_TYPE_ITEM = 0
+        const val VIEW_TYPE_LOADING = 1
+    }
+
+}
