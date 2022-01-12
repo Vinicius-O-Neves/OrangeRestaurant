db7d77db42c97a90068c25bc56916e2ada4d5954
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-12 12:11:52 -0300

    Update AuthDataStoreRepository.kt

--- app/src/main/java/app/dealux/orangerestaurant/repository/AuthDataStoreRepository.kt
+++ app/src/main/java/app/dealux/orangerestaurant/repository/AuthDataStoreRepository.kt
@@ -6,15 +6,15 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
-import app.dealux.orangerestaurant.constants.DataStoreConstants
+import app.dealux.orangerestaurant.constants.OrangeRestaurantConstants
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
-private const val preferenceName = DataStoreConstants.AUTH.TYPE_OF_AUTH_PREFERENCE
-private const val preferenceKey = DataStoreConstants.AUTH.AUTH_PREFERENCE_KEY
+private const val preferenceName = OrangeRestaurantConstants.AUTH.TYPE_OF_AUTH_PREFERENCE
+private const val preferenceKey = OrangeRestaurantConstants.AUTH.AUTH_PREFERENCE_KEY
 
 class AuthDataStoreRepository(val context: Context) {
 
