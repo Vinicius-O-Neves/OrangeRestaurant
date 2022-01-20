7ff74700b121d9e6bbf0cef999768750aa7f1fbc
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-20 20:15:54 -0300

    Created MeatPointModel

---
+++ app/src/main/java/app/dealux/orangerestaurant/data/model/MeatPointModel.kt
@@ -0,0 +1,37 @@
+package app.dealux.orangerestaurant.data.model
+
+import java.io.Serializable
+
+data class MeatPointModel (
+    val id: Int,
+    val meatPointName: String,
+    val meatPointImage: String,
+) : Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        if (javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as AddIngredientModel
+
+        if (id != other.id) {
+            return false
+        }
+        if (meatPointName != other.itemName) {
+            return false
+        }
+        if (meatPointImage != other.itemImage) {
+            return false
+        }
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + meatPointName.hashCode()
+        result = 31 * result + meatPointImage.hashCode()
+        return result
+    }
+
+}
