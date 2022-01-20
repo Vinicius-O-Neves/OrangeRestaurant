2fa12d8e8e6a2c8805ac9d8cb7176a1b766095ca
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-20 20:13:52 -0300

    Create AddIngredientDifferUtil.kt

---
+++ app/src/main/java/app/dealux/orangerestaurant/adapter/itemstoadd/AddIngredientDifferUtil.kt
@@ -0,0 +1,37 @@
+package app.dealux.orangerestaurant.adapter.itemstoadd
+
+import androidx.recyclerview.widget.DiffUtil
+import app.dealux.orangerestaurant.data.model.AddIngredientModel
+
+class AddIngredientDifferUtil(
+    private val oldList: List<AddIngredientModel>,
+    private val newList: List<AddIngredientModel>,
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return when {
+            oldList[oldItemPosition].id != newList[newItemPosition].id-> {
+                false
+            }
+            oldList[oldItemPosition].itemName != newList[newItemPosition].itemName -> {
+                false
+            }
+            oldList[oldItemPosition].itemImage != newList[newItemPosition].itemImage -> {
+                false
+            }
+            else -> true
+        }
+    }
+
+}
