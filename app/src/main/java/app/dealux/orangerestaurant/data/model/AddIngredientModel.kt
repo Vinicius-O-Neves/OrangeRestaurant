58655691f04874cc1019f3930f51f613a17cd701
Vinicius Neves <vinicius.neeves@outlook.com>
2022-01-20 20:14:51 -0300

    Created AddIngredientMOdel

---
+++ app/src/main/java/app/dealux/orangerestaurant/data/model/AddIngredientModel.kt
@@ -0,0 +1,37 @@
+package app.dealux.orangerestaurant.data.model
+
+import java.io.Serializable
+
+data class AddIngredientModel(
+    val id: Int,
+    val itemName: String,
+    val itemImage: String,
+) : Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        if (javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as AddIngredientModel
+
+        if (id != other.id) {
+            return false
+        }
+        if (itemName != other.itemName) {
+            return false
+        }
+        if (itemImage != other.itemImage) {
+            return false
+        }
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + itemName.hashCode()
+        result = 31 * result + itemImage.hashCode()
+        return result
+    }
+
+}
