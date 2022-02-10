bca5360263a39c9214731de742963e85456ba450
Vinicius Neves <vinicius.neeves@outlook.com>
2022-02-10 17:49:43 -0300

    Update AuthDataStoreRepository.kt

--- app/src/main/java/app/dealux/orangerestaurant/repository/AuthDataStoreRepository.kt
+++ app/src/main/java/app/dealux/orangerestaurant/repository/AuthDataStoreRepository.kt
@@ -6,12 +6,13 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
-import app.dealux.orangerestaurant.constants.OrangeRestaurantConstants
+import app.dealux.orangerestaurant.OrangeRestaurantConstants
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import javax.inject.Singleton
 
 private const val preferenceName = OrangeRestaurantConstants.AUTH.TYPE_OF_AUTH_PREFERENCE
 private const val preferenceKey = OrangeRestaurantConstants.AUTH.AUTH_PREFERENCE_KEY
@@ -20,6 +21,7 @@ class AuthDataStoreRepository(val context: Context) {
 
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(preferenceName)
 
+    @Singleton
     suspend fun save(value: Boolean) {
         CoroutineScope(Dispatchers.IO).launch {
             val dataStoreKey = booleanPreferencesKey(preferenceKey)
@@ -29,6 +31,7 @@ class AuthDataStoreRepository(val context: Context) {
         }
     }
 
+    @Singleton
     fun read(): Flow<Boolean?> {
         val dataStoreKey = booleanPreferencesKey(preferenceKey)
         return context.dataStore.data.map { preference ->
