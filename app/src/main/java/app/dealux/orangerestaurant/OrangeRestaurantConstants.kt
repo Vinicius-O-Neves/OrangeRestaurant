23fa5c7a1b02281fb95a1d496d090deddd5b8070
Vinicius Neves <vinicius.neeves@outlook.com>
2022-02-10 17:49:33 -0300

    Create OrangeRestaurantConstants.kt

---
+++ app/src/main/java/app/dealux/orangerestaurant/OrangeRestaurantConstants.kt
@@ -0,0 +1,15 @@
+package app.dealux.orangerestaurant
+
+class OrangeRestaurantConstants private constructor() {
+
+    object AUTH {
+        const val TYPE_OF_AUTH_PREFERENCE = "type_of_auth_preference"
+        const val AUTH_PREFERENCE_KEY = "154"
+    }
+
+    object TABLE {
+        const val TABLE_NUMBER_PREFERENCE = "type_Of_added_preference"
+        const val TABLE_NUMBER_KEY = "145"
+    }
+
+}
