884bfe19d074134fbb8eef67f847443e86310096
Vinicius Neves <vinicius.neeves@outlook.com>
2022-02-10 17:49:14 -0300

    Update MainModule.kt

--- app/src/main/java/app/dealux/orangerestaurant/ui/di/MainModule.kt
+++ app/src/main/java/app/dealux/orangerestaurant/ui/di/MainModule.kt
@@ -2,8 +2,10 @@ package app.dealux.orangerestaurant.ui.di
 
 import androidx.lifecycle.ViewModel
 import app.dealux.orangerestaurant.di.ViewModelKey
-import app.dealux.orangerestaurant.ui.foodandcategoryfragment.FoodAndCategoryViewModel
-import app.dealux.orangerestaurant.ui.splashscreen.SplashScreenViewModel
+import app.dealux.orangerestaurant.ui.fragments.viewmodel.FoodFragmentViewModel
+import app.dealux.orangerestaurant.ui.activitys.viewmodel.SplashScreenViewModel
+import app.dealux.orangerestaurant.ui.fragments.viewmodel.CartFragmentViewModel
+import app.dealux.orangerestaurant.ui.fragments.viewmodel.SelectTableFragmentViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -14,11 +16,21 @@ interface MainModule {
     @Binds
     @IntoMap
     @ViewModelKey(SplashScreenViewModel::class)
-    fun bindSplashScreenViewModel(viewModel: SplashScreenViewModel): ViewModel
+    fun bindSplashScreenViewModel(viewModel: SplashScreenViewModel) : ViewModel
+    
+    @Binds
+    @IntoMap
+    @ViewModelKey(FoodFragmentViewModel::class)
+    fun bindFoodFragmentViewModel(viewModel: FoodFragmentViewModel) : ViewModel
 
     @Binds
     @IntoMap
-    @ViewModelKey(FoodAndCategoryViewModel::class)
-    fun bindFoodAndCategoryViewModel(viewModel: FoodAndCategoryViewModel): ViewModel
+    @ViewModelKey(CartFragmentViewModel::class)
+    fun bindCartFragmentViewModel(viewModel: CartFragmentViewModel) : ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SelectTableFragmentViewModel::class)
+    fun bindSelectTableFragmentViewModel(viewModel: SelectTableFragmentViewModel) : ViewModel
 
 }
