8124d7cf210ada941060982119c4932885402949
Vinicius Neves <vinicius.neeves@outlook.com>
2022-02-10 17:51:43 -0300

    Database migration

--- app/src/main/java/app/dealux/orangerestaurant/data/sqlite/OrderDataBase.kt
+++ app/src/main/java/app/dealux/orangerestaurant/data/sqlite/OrderDataBase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [OrderEntity::class], version = 2, exportSchema = false)
+@Database(entities = [OrderEntity::class], version = 4, exportSchema = false)
 abstract class OrderDataBase: RoomDatabase() {
 
     abstract fun orderDao(): OrderDao
@@ -15,6 +15,12 @@ abstract class OrderDataBase: RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: OrderDataBase? = null
+
+        val MIGRATION_3_4 = object : Migration(3,4) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE orders_table ADD COLUMN meatPoint TEXT NOT NULL DEFAULT ('')")
+            }
+        }
 
         fun getDatabase(context: Context): OrderDataBase {
             val tempInstance = INSTANCE
@@ -26,8 +32,9 @@ abstract class OrderDataBase: RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     OrderDataBase::class.java,
-                    "orders_database"
+                    "orders_database_3"
                 )
+                    .addMigrations(MIGRATION_3_4)
                     .build()
                 INSTANCE = instance
                 return instance
