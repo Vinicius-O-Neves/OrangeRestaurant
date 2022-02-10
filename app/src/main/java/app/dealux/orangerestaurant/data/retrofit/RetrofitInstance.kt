10568e3ef59f6501d0fde5885cc8da8e485aa7af
Vinicius Neves <vinicius.neeves@outlook.com>
2022-02-10 17:52:13 -0300

    Nothing changed

--- app/src/main/java/app/dealux/orangerestaurant/data/retrofit/RetrofitInstance.kt
+++ app/src/main/java/app/dealux/orangerestaurant/data/retrofit/RetrofitInstance.kt
@@ -2,21 +2,12 @@ package app.dealux.orangerestaurant.data.retrofit
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object RetrofitInstance {
 
     val api: OrangeRestaurantApi by lazy {
         Retrofit.Builder()
             .baseUrl(OrangeRestaurantApi.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(OrangeRestaurantApi::class.java)
-    }
-
-    val orderApi: OrangeRestaurantApi by lazy {
-        Retrofit.Builder()
-            .baseUrl(OrangeRestaurantApi.ORDER_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(OrangeRestaurantApi::class.java)
