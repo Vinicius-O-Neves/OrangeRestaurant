c0bdeaf3634030ccdf583b5d94859a08d29172a6
Vinicius Neves <vinicius.neeves@outlook.com>
2022-02-16 11:51:20 -0300

    Added fun to inject in FoodAndCategoryFragment

--- app/src/main/java/app/dealux/orangerestaurant/ui/di/MainComponent.kt
+++ app/src/main/java/app/dealux/orangerestaurant/ui/di/MainComponent.kt
@@ -4,6 +4,7 @@ import app.dealux.orangerestaurant.ui.fragments.view.FoodFragment
 import app.dealux.orangerestaurant.ui.activitys.view.MainActivity
 import app.dealux.orangerestaurant.ui.activitys.view.SplashScreen
 import app.dealux.orangerestaurant.ui.fragments.view.CartFragment
+import app.dealux.orangerestaurant.ui.fragments.view.FoodAndCategoryFragment
 import app.dealux.orangerestaurant.ui.fragments.view.SelectTableFragment
 import dagger.Subcomponent
 
@@ -17,6 +18,7 @@ interface MainComponent {
 
     fun inject(activity: SplashScreen)
     fun inject(activity: MainActivity)
+    fun inject(fragment: FoodAndCategoryFragment)
     fun inject(fragment: FoodFragment)
     fun inject(fragment: CartFragment)
     fun inject(fragment: SelectTableFragment)
